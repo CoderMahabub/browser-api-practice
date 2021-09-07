2b5177332ec167e49983b6cf6d1a84bb78fa99f9
CoderMahabub <codermahabub@gmail.com>
2021-09-07 22:34:05 +0600

    Theory Part

--- checkList.js
+++ checkList.js
@@ -15,10 +15,10 @@ alert(result); */
 
 // ৩. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও
 
-/* const confimation = confirm('Do you want to see the website location?');
+const confimation = confirm('Do you want to see the website location?');
 if (confimation) {
-    console.log(location.href);
-} */
+    location.href = 'https://google.com';
+}
 
 // ৪. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে লিখে ফেলো। 
 
@@ -62,3 +62,13 @@ Blockable by users: Yes
 Editable by users: Yes */
 
 
+// [হোম ওয়ার্ক]
+// ৬. জাভাস্ক্রিপ্ট এর কোড কিভাবে ব্রাউজারের মধ্যে রান করে। দরকার হলে গুগল বা ইউটিউবে সার্চ দিয়ে একটু ভালো করে দেখে রাখো।
+
+
+
+// ৭. জাভাস্ক্রিপ্ট এর কোন কোন জিনিস asynchronous সেটার একটা লিষ্ট গুগলে সার্চ দিয়ে বের করে ফেলো। এবং এই asynchronous বলতে কি বুঝায় সেটাও দেখে রাখো। 
+
+
+
+// ৮. event loop লুপ কি জিনিস। এই রিলেটেড পুরা জিনিসটা আজকে অনেকেই বুঝবে না। তাও আরেকবার দেখে রাখবে। 
